b46b747137ee30d6711fc429e3efbd0d1fba1714
sandmark <sandmark.m@gmail.com>
2019-02-24 08:52:29 +0900

    Add `chain` macro, works like `..` macro.

--- src/reader/macros.clj
+++ src/reader/macros.clj
@@ -4,3 +4,12 @@
   (list 'if expr nil form))
 
 ;; (macroexpand-1 '(unless false (println "This should print.")))
+
+(defmacro chain
+  ([x form] (list '. x form))
+  ([x form & more]
+   (concat (list 'chain (list '. x form)) more)))
+
+(macroexpand-1 '(chain arm getHand))
+(macroexpand-1 '(chain arm getHand getFinger))
+(macroexpand '(chain arm getHand getFinger))
