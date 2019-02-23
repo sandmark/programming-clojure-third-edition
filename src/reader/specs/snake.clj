1eb49a3afdedc9895ae43c436407d634e995716e
sandmark <sandmark.m@gmail.com>
2019-02-23 12:24:04 +0900

    Add specs of ::game-object

--- src/reader/specs/snake.clj
+++ src/reader/specs/snake.clj
@@ -1,8 +1,11 @@
 (ns reader.specs.snake
   (:require [clojure.spec.alpha :as s]
             [clojure.spec.test.alpha :as stest]
-            [reader.snake :as snake]))
+            [examples.import-static :as import-static]
+            [reader.snake :as snake])
+  (:import java.awt.Color))
 
+(import-static/import-static java.awt.event.KeyEvent VK_LEFT VK_RIGHT VK_UP VK_DOWN)
 
 (s/def ::point
   (s/tuple (s/int-in 0 snake/width)
@@ -10,6 +13,18 @@
 
 (s/def ::rectangle
   (s/coll-of int? :count 4))
+
+(s/def ::color (partial instance? Color))
+(s/def ::type #{:apple :snake})
+(s/def ::location ::point)
+(s/def ::body (s/and list? (s/coll-of ::point)))
+(s/def ::dir (s/or :left  (get snake/dirs VK_LEFT)
+                   :right (get snake/dirs VK_RIGHT)
+                   :up    (get snake/dirs VK_UP)
+                   :down  (get snake/dirs VK_DOWN)))
+
+(s/def ::game-object
+  (s/keys :req [::color ::type (or ::location (and ::body ::dir))]))
 
 (s/fdef snake/add-points
   :args (s/+ ::point)
